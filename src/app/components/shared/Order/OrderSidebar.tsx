ea1f556d0cdfe4e547f39ca1537a2d15aa4f16d1
José Brito <josealbritose@gmail.com>
2024-04-04 16:53:55 -0300

    Sidebar order

--- src/app/components/shared/Order/OrderSidebar.tsx
+++ src/app/components/shared/Order/OrderSidebar.tsx
@@ -1,8 +1,16 @@
+import { getCategories } from "@/app/api/controllers/category"
+import CategoryIcon from "../ui/CategoryIcon";
 
-export default function OrderSidebar() {
+export default async function OrderSidebar() {
+
+  const categories = await getCategories();
+
   return (
     <aside className="md:w-72 md:h-screen bg-white">
-     <h1> OrderSidebar</h1>
+     {/* <h1> OrderSidebar</h1> */}
+     <nav className="mt-10">
+      {categories.map((category)=><CategoryIcon key={category.id} category={category}/>)}
+     </nav>
     </aside>
   )
 }
