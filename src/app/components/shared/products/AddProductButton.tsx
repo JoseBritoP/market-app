5da378db49579d29707abc359a0238897aba7537
José Brito <josealbritose@gmail.com>
2024-04-04 17:35:33 -0300

    AddProductToOrder

---
+++ src/app/components/shared/products/AddProductButton.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useStore } from "@/app/services/store"
+import { Product } from "@prisma/client"
+
+interface AddProductButtonProps {
+  product:Product
+}
+
+export default function AddProductButton({product}:AddProductButtonProps) {
+
+  const { addToOrder } = useStore();
+
+  return (
+    <button className="bg-sky-600 hover:bg-sky-700 transition-colors 300 ease-in-out text-white w-full mt-5 p-3 uppercase font-semibold cursor-pointer"
+      onClick={()=>addToOrder(product)}
+    >Agree</button>
+  )
+}
