23e9be5b7f0d4d0c839e02da2955da8f5febf9cc
José Brito <josealbritose@gmail.com>
2024-04-04 17:36:30 -0300

    ProductDetailsOrder rendering

--- src/app/components/shared/Order/OrderSummary.tsx
+++ src/app/components/shared/Order/OrderSummary.tsx
@@ -1,9 +1,19 @@
+"use client"
 import React from 'react'
-
+import { useStore } from '@/app/services/store';
+import ProductDetailsOrder from './ProductDetailsOrder';
 export default function OrderSummary() {
+
+  const { order } = useStore();
   return (
     <aside className='lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5'>
       <h1 className='text-2xl text-center font-semibold'>My order</h1>
+      {!order.length ?  <p className='text-center my-10'>The cart is empty</p> 
+      : (
+      <div className='mt-5'>
+        {order.map((item)=>(<ProductDetailsOrder key={item.id} item={item}/>))}
+      </div>
+      )}
     </aside>
   )
 }
