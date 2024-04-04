ab20fa20e2b892f6ad0556204b1376475d10f4bd
José Brito <josealbritose@gmail.com>
2024-04-04 17:06:48 -0300

    CategoryIcon ui

--- src/app/components/shared/ui/CategoryIcon.tsx
+++ src/app/components/shared/ui/CategoryIcon.tsx
@@ -1,11 +1,15 @@
+"use client"
 import { Category } from "@prisma/client"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 
 type CategoryIconProps = {
   category:Category
 }
 export default function CategoryIcon({category}:CategoryIconProps) {
+  const params = useParams<{category:string}>();
+  console.log(params)
   return (
     // <div className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b`}>
     //   <div className="w-16 h-16 relative">
@@ -15,7 +19,9 @@ export default function CategoryIcon({category}:CategoryIconProps) {
     //   </div>
     //   <Link href={`/order/${category.slug}`} className="text-xl font-semibold">{category.name}</Link>
     // </div>
-    <Link href={`/order/${category.slug}`} className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b hover:bg-slate-200 `}>
+    <Link href={`/order/${category.slug}`} className={`flex items-center gap-4 w-full border-t border-gray-200 p-3 last-of-type:border-b hover:bg-slate-200 hover:text-black 
+    ${category.slug === params.category ? 'bg-amber-500 text-white' : ''}
+    `}>
       <div className="w-16 h-16 relative">
         <Image src={`/icon_${category.slug}.svg`} alt={`${category.name}`}
         fill
