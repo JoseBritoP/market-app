17657ee52398cc0b4282ed8e25088c73882e9cad
José Brito <josealbritose@gmail.com>
2024-04-04 12:08:16 -0300

    Order components innit

---
+++ src/app/components/shared/Order/OrderSidebar.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderSidebar() {
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+     <h1> OrderSidebar</h1>
+    </aside>
+  )
+}
---
+++ src/app/components/shared/Order/OrderSummary.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OrderSummary() {
+  return (
+    <aside className='lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5'>
+      <h1 className='text-2xl text-center font-semibold'>My order</h1>
+    </aside>
+  )
+}
