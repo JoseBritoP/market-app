3579d3cdbed385afa028edbe069c7a3534b8e4f2
José Brito <josealbritose@gmail.com>
2024-04-04 17:00:51 -0300

    Add h1

--- src/app/(root)/order/[category]/page.tsx
+++ src/app/(root)/order/[category]/page.tsx
@@ -5,10 +5,11 @@ import ProductCard from '@/app/components/shared/products/ProductCard'
 export default async function OrderPage({params}:{params:{category:string}}) {
   const products = await getProducts(params.category)
   return (
-    <div>
+    <section>
+      <h1 className='text-2xl my-10 font-semibold'>Choose and customize your order below </h1>
       <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-center'>
         {products.map((product)=>(<ProductCard key={product.id} product={product}/>))}
       </div>
-    </div> 
+    </section> 
   )
 }
