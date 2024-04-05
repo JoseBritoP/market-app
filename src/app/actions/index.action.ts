d7a63c0c34badf580bfa2aed999a22f4d4964e5e
José Brito <josealbritose@gmail.com>
2024-04-05 12:10:30 -0300

    action - createOrder

--- src/app/actions/index.action.ts
+++ src/app/actions/index.action.ts
@@ -1,5 +1,26 @@
 "use server";
 
-export async function createOrder() {
-  console.log('Index-create')
+import { orderSchema } from "../schema";
+import { OrderItem } from "../types";
+import { createOrderDB } from "../api/controllers/order";
+
+interface DataProps {
+    clientName: FormDataEntryValue | null;
+    total:number,
+    order:OrderItem[]
+}
+
+export async function createOrder(data:DataProps) {
+  const result = orderSchema.safeParse(data);
+  if(!result.success){
+    return {
+      errors:result.error.issues
+    }
+  };
+
+  try {
+    await createOrderDB(result.data)
+  } catch (error) {
+    console.log('error',error)
+  }
 }
