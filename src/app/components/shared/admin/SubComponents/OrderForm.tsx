869f8a6ff2087a5a3ac07c88b652df3519a165a8
José Brito <josealbritose@gmail.com>
2024-04-05 14:11:51 -0300

    Get orderId  with FormData

--- src/app/components/shared/admin/SubComponents/OrderForm.tsx
+++ src/app/components/shared/admin/SubComponents/OrderForm.tsx
@@ -1,8 +1,12 @@
 import { completeOrder } from '@/app/actions/completeOrder.action.'
 
-export default function OrderForm() {
+interface Props {
+  orderId:number
+}
+export default function OrderForm({orderId}:Props) {
   return (
     <form action={completeOrder}>
+      <input type='hidden' value={orderId} name='order_id'/>
       <input
           type="submit"
           className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
