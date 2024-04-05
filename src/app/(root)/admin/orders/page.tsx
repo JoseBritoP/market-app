c9e52de70fa6d320cf6e3752de6ab3bf9dc0c225
José Brito <josealbritose@gmail.com>
2024-04-05 13:57:43 -0300

    Admin/Orders/Page

--- src/app/(root)/admin/orders/page.tsx
+++ src/app/(root)/admin/orders/page.tsx
@@ -1,4 +1,5 @@
 import { getPendingOrders } from '@/app/api/controllers/order'
+import OrderCard from '@/app/components/shared/admin/orders/OrderCard';
 import Heading from '@/app/components/shared/ui/Heading'
 import React from 'react'
 
@@ -7,6 +8,14 @@ export default async function AdminOrdersPage() {
   return (
     <div>
       <Heading title='Manage orders'/>
+      { orders ? 
+        (
+          <div className='gird grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5'>
+            {orders.map((order)=>(<OrderCard key={order.id} order={order}/>))}
+          </div>
+        ) 
+        : <p>No orders</p>
+      }
     </div>
   )
 }
