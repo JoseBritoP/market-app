b009e2004f7a28509d464a693904305f72e29267
José Brito <josealbritose@gmail.com>
2024-04-05 16:17:54 -0300

    Pagination render

--- src/app/(root)/admin/products/page.tsx
+++ src/app/(root)/admin/products/page.tsx
@@ -1,10 +1,23 @@
-import Heading from '@/app/components/shared/ui/Heading'
 import React from 'react'
+import Heading from '@/app/components/shared/ui/Heading'
+import { getProductsTotal, getProductsWithCategories } from '@/app/api/controllers/product'
+import ProductTable from '@/app/components/shared/admin/products/ProductTable';
+import ProductsPagination from '@/app/components/shared/admin/products/ProductsPagination';
 
-export default function ProductPage() {
+
+export default async function ProductPage({searchParams}:{searchParams:{page:string}}) {
+  const page = +searchParams.page || 1;
+  const pageSize = 10;
+  const productsData =  getProductsWithCategories(page,pageSize);
+  const totalProductsData = getProductsTotal();
+  const [products,totalProducts] = await Promise.all([productsData,totalProductsData])
+  const totalPages = Math.ceil(totalProducts/pageSize);
+
   return (
     <section>
       <Heading title='Manage Products'/>
+      <ProductTable products={products}/>
+      <ProductsPagination page={page} totalPages={totalPages}/>
     </section>
   )
 }
