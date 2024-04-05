9be1f72160b67a2df5a70eb703c2f55f1d124d7a
José Brito <josealbritose@gmail.com>
2024-04-05 18:17:25 -0300

    ProductPage

--- src/app/(root)/admin/products/new/page.tsx
+++ src/app/(root)/admin/products/new/page.tsx
@@ -1,9 +1,15 @@
+import ProductEditForm from '@/app/components/shared/admin/products/ProductEditForm'
+import ProductForm from '@/app/components/shared/admin/products/ProductForm'
+import Heading from '@/app/components/shared/ui/Heading'
 import React from 'react'
 
 export default function NewProductPage() {
   return (
     <section>
-      <h1>New Product Page</h1>
+      <Heading title='New Product'/>
+      <ProductForm>
+        <ProductEditForm/>
+      </ProductForm>
     </section>
   )
 }
