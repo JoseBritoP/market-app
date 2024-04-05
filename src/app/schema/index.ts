9f2d3b197df96838da14e07649c487d3f9f277ea
José Brito <josealbritose@gmail.com>
2024-04-05 11:33:00 -0300

    Schemes

--- src/app/schema/index.ts
+++ src/app/schema/index.ts
@@ -1,5 +1,15 @@
 import z from 'zod';
 
+const itemSchema = z.object({
+  id:z.number(),
+  name:z.string(),
+  price:z.number(),
+  quantity:z.number(),
+  subtotal:z.number()
+})
+
 export const orderSchema = z.object({
   clientName:z.string().min(1,"Your name is required"),
+  total:z.number().min(1,'There are errors in the order'),
+  order:z.array(itemSchema)
 })
