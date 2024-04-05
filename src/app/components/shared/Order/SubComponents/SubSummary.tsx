c5c3832ff5319bab5341b5e39f4561ebc040a403
José Brito <josealbritose@gmail.com>
2024-04-05 09:57:49 -0300

    SubSummary Component

---
+++ src/app/components/shared/Order/SubComponents/SubSummary.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React, { useMemo } from 'react'
+import ProductDetailsOrder from '../ProductDetailsOrder'
+import { formatCurrency } from '@/app/utils'
+import { useOrder } from '@/app/services/store'
+
+export default function SubSummary() {
+  const { order } = useOrder();
+  const total = useMemo(()=>(order.reduce((total,item)=>total+(item.quantity*item.price),0)),[order])
+  return (
+    <>
+      {!order.length ?  <p className='text-center my-10'>The cart is empty</p> 
+      : (
+      <div className='mt-5 flex flex-col gap-y-2'>
+        {order.map((item)=>(<ProductDetailsOrder key={item.id} item={item}/>))}
+        <p className='text-2xl mt-8 text-center p-2 bg-white rounded-md border shadow'>Total: <span className='font-semibold'>{formatCurrency(total)}</span></p>
+        <form></form>
+      </div>
+      )}
+    </>
+  )
+}
