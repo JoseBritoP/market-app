a9d080a1d55af54287669fa74dfbc03c14e1edbf
José Brito <josealbritose@gmail.com>
2024-04-05 16:47:36 -0300

    ProductSearchForm render

--- src/app/(root)/admin/products/page.tsx
+++ src/app/(root)/admin/products/page.tsx
@@ -4,6 +4,8 @@ import { getProductsTotal, getProductsWithCategories } from '@/app/api/controlle
 import ProductTable from '@/app/components/shared/admin/products/ProductTable';
 import ProductsPagination from '@/app/components/shared/admin/products/ProductsPagination';
 import { redirect } from 'next/navigation';
+import Link from 'next/link';
+import ProductSearchForm from '@/app/components/shared/admin/products/ProductSearchForm';
 
 
 export default async function ProductPage({searchParams}:{searchParams:{page:string}}) {
@@ -19,6 +21,10 @@ export default async function ProductPage({searchParams}:{searchParams:{page:str
   return (
     <section>
       <Heading title='Manage Products'/>
+      <div className='flex flex-col gap-5 lg:flex-row lg:justify-between'>
+        <Link className='bg-amber-400 w-full lg:w-auto text-xl px-10 rounded-md text-white hover:bg-amber-500 transition-colors ease-in-out py-3 text-center font-semibold' href={'/admin/products/new'}>Create Product</Link>
+        <ProductSearchForm/>
+      </div>
       <ProductTable products={products}/>
       <ProductsPagination page={page} totalPages={totalPages}/>
     </section>
