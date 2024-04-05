a2338d28be2eb28db26756fa57ffe629617551c2
José Brito <josealbritose@gmail.com>
2024-04-05 18:16:52 -0300

    ProductCreateScheme

--- src/app/schema/index.ts
+++ src/app/schema/index.ts
@@ -17,3 +17,20 @@ export const orderSchema = z.object({
 export const SearchSchema = z.object({
   search:z.string().trim().min(1,{message:'The search is not valid'})
 })
+
+export const ProductSchema = z.object({
+  name: z.string()
+      .trim()
+      .min(1, { message: 'The product name is required'}),
+  price: z.string()
+      .trim()
+      .transform((value) => parseFloat(value)) 
+      .refine((value) => value > 0, { message: 'Price invalid' })
+      .or(z.number().min(1, {message: 'The category is required' })),
+  categoryId: z.string()
+      .trim()
+      .transform((value) => parseInt(value)) 
+      .refine((value) => value > 0, { message: 'The category is required' })
+      .or(z.number().min(1, {message: 'The category is required' })),
+  image:z.string().min(1,{message:'The image is required'})
+})
