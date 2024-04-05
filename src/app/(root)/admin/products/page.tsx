51e1657f65bac3a299b2346362b7cb43ded1f439
José Brito <josealbritose@gmail.com>
2024-04-05 16:31:03 -0300

    Pagination fix issues

--- src/app/(root)/admin/products/page.tsx
+++ src/app/(root)/admin/products/page.tsx
@@ -3,15 +3,18 @@ import Heading from '@/app/components/shared/ui/Heading'
 import { getProductsTotal, getProductsWithCategories } from '@/app/api/controllers/product'
 import ProductTable from '@/app/components/shared/admin/products/ProductTable';
 import ProductsPagination from '@/app/components/shared/admin/products/ProductsPagination';
+import { redirect } from 'next/navigation';
 
 
 export default async function ProductPage({searchParams}:{searchParams:{page:string}}) {
   const page = +searchParams.page || 1;
   const pageSize = 10;
+  if(page < 0) redirect('/admin/products?page=1')
   const productsData =  getProductsWithCategories(page,pageSize);
   const totalProductsData = getProductsTotal();
   const [products,totalProducts] = await Promise.all([productsData,totalProductsData])
   const totalPages = Math.ceil(totalProducts/pageSize);
+  if(page > totalPages) redirect('/admin/products?page=1')
 
   return (
     <section>
--- src/app/components/shared/admin/products/ProductsPagination.tsx
+++ src/app/components/shared/admin/products/ProductsPagination.tsx
@@ -9,38 +9,32 @@ interface ProductsPaginationProps {
 export default function ProductsPagination({page,totalPages}:ProductsPaginationProps) {
   const disabledLeft = page > 1;
   const disabledRight = page < totalPages;
+  const pages = Array.from({length:totalPages},(_,i)=>i+1)
   return (
     <nav className='flex justify-center py-10'>
-        <Link href={`/admin/products?page=${1}`}>
-          <button
-          className='disabled:opacity-70 disabled:bg-gray-300 border bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0 '
-          disabled={!disabledLeft}
-          >
-            {`<<`}
-          </button>
-        </Link>
         <Link href={`/admin/products?page=${page-1}`}>
           <button
-          className='disabled:opacity-70 disabled:bg-gray-300 border bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0 '
+          className='disabled:opacity-70 disabled:bg-gray-300 rounded-md border bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0 '
           disabled={!disabledLeft}
           >
             &laquo;
           </button>
         </Link>
+        {pages.map((currentPage)=>(
+          <Link key={currentPage} href={`/admin/products?page=${currentPage}`}>
+            <button
+              className={`${page === currentPage ? 'font-black bg-gray-200' : 'bg-white'} rounded-md border  px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}
+            >
+              {currentPage}
+            </button>
+            </Link>
+        ))}
         <Link href={`/admin/products?page=${page+1}`}>
           <button
-          className='disabled:opacity-70 disabled:bg-gray-300 border bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0 '
+          className='disabled:opacity-70 disabled:bg-gray-300 rounded-md border bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0 '
           disabled={!disabledRight}
           >
             &raquo;
-          </button>
-        </Link>
-        <Link href={`/admin/products?page=${totalPages}`}>
-          <button
-          className='disabled:opacity-70 disabled:bg-gray-300 border bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0 '
-          disabled={!disabledRight}
-          >
-            {`>>`}
           </button>
         </Link>
     </nav>
