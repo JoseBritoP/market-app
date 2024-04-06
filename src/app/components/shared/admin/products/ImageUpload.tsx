e323753bdf3ced7cedd97ba32e3927808fbf4aba
José Brito <josealbritose@gmail.com>
2024-04-06 07:02:10 -0300

    Styles

--- src/app/components/shared/admin/products/ImageUpload.tsx
+++ src/app/components/shared/admin/products/ImageUpload.tsx
@@ -26,7 +26,7 @@ export default function ImageUpload() {
             <label className="text-slate-800">Product Image</label>
             <div
               onClick={()=>open()}
-            className="relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100">
+              className="rounded-md relative cursor-pointer hover:opacity-70 transition p-10 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600 bg-slate-100">
               <TbPhotoPlus size={50}/>
               <p className="text-lg font-semibold">Agree image</p>
               {imageUrl && 
