42126a4b58b6a45531b9f4e963cde40bb7ad6828
José Brito <josealbritose@gmail.com>
2024-04-06 07:57:47 -0300

    EditFormPage

--- src/app/(root)/admin/products/[productId]/edit/page.tsx
+++ src/app/(root)/admin/products/[productId]/edit/page.tsx
@@ -1,4 +1,7 @@
 import { getProductById } from '@/app/api/controllers/product'
+import ProductEditForm from '@/app/components/shared/admin/products/ProductEditForm';
+import ProductEditFormC from '@/app/components/shared/admin/products/ProductEditFormC';
+import Heading from '@/app/components/shared/ui/Heading';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
@@ -9,8 +12,11 @@ export default async function ProductEditPage({params}:{params:{productId:string
     notFound()
   }
   return (
-    <div>
-      Page
-    </div>
+    <>
+      <Heading title={`Edit product: ${product.name}`}/>
+      <ProductEditFormC>
+        <ProductEditForm product={product}/>
+      </ProductEditFormC>
+    </>
   )
 }
