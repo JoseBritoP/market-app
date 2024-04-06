cca81db42326980e58b0913e38e3afc2d53cce71
José Brito <josealbritose@gmail.com>
2024-04-06 06:37:01 -0300

    style

--- src/app/components/shared/admin/AdminRoute.tsx
+++ src/app/components/shared/admin/AdminRoute.tsx
@@ -10,7 +10,7 @@ export default function AdminRoute({blank,text,url}:AdminRouteProps) {
   const pathname = usePathname();
   const isActive = pathname.startsWith(url);
   return (
-    <Link className={`${isActive ? "bg-amber-400 text-white hover:text-white hover:bg-amber-500" : "" } hover:bg-gray-100 font-semibold text-lg border-t- border-gray-200 p-3 last-of-type:border-b`} href={url} target={blank ? '_blank' : ''}>
+    <Link className={`${isActive ? "bg-amber-400 text-white hover:text-white hover:bg-amber-500" : "" } font-semibold text-lg border-t- border-gray-200 p-3 last-of-type:border-b`} href={url} target={blank ? '_blank' : ''}>
       {text}
     </Link>
   )
