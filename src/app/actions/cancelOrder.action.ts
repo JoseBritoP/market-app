862c2d708b2673abfce6646f5bd7e56436c23ae8
José Brito <josealbritose@gmail.com>
2024-04-07 17:00:17 -0300

    Agree cancel action in order

--- src/app/actions/cancelOrder.action.ts
+++ src/app/actions/cancelOrder.action.ts
@@ -1,14 +1,14 @@
 "use server"
 import { revalidatePath } from "next/cache";
 
-import { orderStatus,orderCompleteStatus } from "../api/controllers/order"
+import { orderCancelStatus } from "../api/controllers/order"
 
-export async function completeOrder(formData:FormData) {
+export async function cancelOrder(formData:FormData) {
   const orderId = formData.get('order_id');
   if(!orderId) return;
 
   try {
-    await orderCompleteStatus(orderId);
+    await orderCancelStatus(orderId);
     revalidatePath('/admin/orders')
     revalidatePath('/orders')
     return
