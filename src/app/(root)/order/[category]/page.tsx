049bf095a692db9b6ceb698d38e5925b59c5b69c
José Brito <josealbritose@gmail.com>
2024-05-10 01:51:51 -0300

    Styles

--- src/app/(root)/order/[category]/page.tsx
+++ src/app/(root)/order/[category]/page.tsx
@@ -9,7 +9,7 @@ export default async function OrderPage({params}:{params:{category:string}}) {
   return (
     <section>
       <Heading title='Choose and customize your order below'/>
-      <div className='grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4 gap-4 items-center'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-4 gap-4 items-center'>
         {products.map((product)=>(<ProductCard key={product.id} product={product}/>))}
       </div>
     </section> 
--- src/app/components/shared/products/ProductCard.tsx
+++ src/app/components/shared/products/ProductCard.tsx
@@ -11,8 +11,12 @@ export default function ProductCard({product}:ProductCardProps) {
   const imagePath = getImagePath(product.image)
   // TODO: Fix styles responsive
   return (
-    <section className="border bg-gray-100 rounded-md shadow-lg">
-      <Image src={imagePath} alt={`Menu ${product.name}`} height={300} width={400} className="object-contain"/>
+    <section className="border bg-gray-100 rounded-md shadow-lg
+    max-w-[400px]
+    sm:max-w-[300px]
+    md:max-w-[404px]
+    ">
+      <Image src={imagePath} alt={`Menu ${product.name}`} height={300} width={400} className="object-contain rounded-t-md" loading="lazy"/>
       <div className="p-5 h-60 flex flex-col justify-center items-center">
         <div className="flex flex-col justify-start items-start w-full h-36">
         <h3 className="text-xl font-semibold h-16 text-balance">{product.name}</h3>
