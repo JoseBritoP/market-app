a27947003b02f998c97969b490609a0926fde548
José Brito <josealbritose@gmail.com>
2024-05-10 01:31:32 -0300

    Styles

--- src/app/components/shared/products/ProductCard.tsx
+++ src/app/components/shared/products/ProductCard.tsx
@@ -14,8 +14,8 @@ export default function ProductCard({product}:ProductCardProps) {
     <section className="border bg-gray-100 rounded-md shadow-lg">
       <Image src={imagePath} alt={`Menu ${product.name}`} height={300} width={400} className="object-contain"/>
       <div className="p-5 h-60 flex flex-col justify-center items-center">
-        <div className="flex flex-col w-full h-36">
-        <h3 className="text-xl font-semibold  text-balance">{product.name}</h3>
+        <div className="flex flex-col justify-start items-start w-full h-36">
+        <h3 className="text-xl font-semibold h-16 text-balance">{product.name}</h3>
         <p className="mt-5 font-bold text-4xl text-amber-500 self-start gap-y-5">{formatCurrency(product.price)}</p>
         </div>
         <AddProductButton product={product}/>
